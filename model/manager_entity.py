3a3d01fd877937ad140c3ee74ed7b38403874c4b
Mousavi <161970487+Mousavi2005@users.noreply.github.com>
2024-05-03 10:26:58 +0330

    Delete model directory

--- model/manager_entity.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-from sqlalchemy.orm import Mapped, mapped_column,relationship
-from sqlalchemy import Column, BIGINT, DateTime, BOOLEAN, BigInteger, Boolean, String, ForeignKey, Integer,VARCHAR
-from sqlalchemy.ext.declarative import declarative_base
-from .base_entity import BaseEntity
-import argparse
-
-
-
